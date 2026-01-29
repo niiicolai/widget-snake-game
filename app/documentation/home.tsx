d8ad590308d76a737938c49c8d2141bd9d169993
Nicolai <niiicolai@gmail.com>
2026-01-29 10:05:47 +0100

    update link in docs

--- app/documentation/home.tsx
+++ app/documentation/home.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
-  const widgetUrl = `http://localhost:5173/#/snake`;
+  const widgetUrl = `https://niiicolai.github.io/widget-snake-game/#/snake`;
 
   return (
     <div className="max-w-4xl mx-auto p-6 bg-white dark:bg-gray-900">
